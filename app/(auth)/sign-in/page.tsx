05f4e54a5a258b314750e0c31457baa9724c2956
ziya yildirim <zia.yildirimm@gmail.com>
2025-02-16 14:13:03 +0300

    creating auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,18 @@
+"use client"
 import React from 'react'
-
+import AuthForm from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validations'
 const SignIn = () => {
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,20 @@
+"use client"
 import React from 'react'
-
+import AuthForm from '@/components/forms/AuthForm'
+import { SignUpSchema } from '@/lib/validations'
 const SignUp = () => {
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+      }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
   )
 }
 
