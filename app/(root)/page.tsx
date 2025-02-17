44b32764689dafe15d1fc8346d780978a7ce97fb
ziya yildirim <zia.yildirimm@gmail.com>
2025-02-17 12:53:10 +0300

    implement filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
-import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import ROUTES from "@/constants/routes";
+
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const questions = [
   {
@@ -24,21 +25,7 @@ const questions = [
     title: "How to learn JavaScript?",
     description: "I want to learn JavaScript, can anyone help me?",
     tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: { _id: "1", name: "John Doe" },
-    upvotes: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-  {
-    _id: "3",
-    title: "How to learn Next.js?",
-    description: "I want to learn Next.js, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
+      { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
     author: { _id: "1", name: "John Doe" },
@@ -50,41 +37,43 @@ const questions = [
 ];
 
 interface SearchParams {
-  searchParams: Promise<{
-    [key: string]: string;
-  }>;
+  searchParams: Promise<{ [key: string]: string }>;
 }
 
-
-
 const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "", filter = "" } = await searchParams;
 
-  const { query = "" } = await searchParams;
-
-  const filteredQuestions = questions.filter((question) => question.title.toLowerCase().includes(query?.toLowerCase() || ""));
-
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
-      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Button className="primary-gradient min-h-[46px] !text-light-900 px-8 py-3">
-          <Link href={ROUTES.ASK_QUESTION}>
-            Ask a question
-          </Link>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-
       <section className="mt-11">
         <LocalSearch
           route="/"
           imgSrc="/icons/search.svg"
-          placeholder="Search for Questions Here..."
+          placeholder="Search questions..."
           otherClasses="flex-1"
         />
       </section>
-
-
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
