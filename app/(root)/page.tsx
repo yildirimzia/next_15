13f3b19bead8c477b4c6b0b58ce5664a0853f6e6
ziya yildirim <zia.yildirimm@gmail.com>
2025-02-17 11:34:09 +0300

    Home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,29 @@
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import ROUTES from "@/constants/routes";
-
 const Home = async () => {
   const session = await auth();
   console.log(session);
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      <form className="px-10 pt-[100px]" action={async (formData) => {
-        "use server"
-        await signOut({ redirectTo: ROUTES.SIGN_IN })
-      }}>
-        <button type="submit">Logout</button>
-      </form>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button className="primary-gradient min-h-[46px] !text-light-900 px-8 py-3">
+          <Link href={ROUTES.ASK_QUESTION}>
+            Ask a question
+          </Link>
+        </Button>
+      </section>
+
+      <section>
+        Local Search
+      </section>
+
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        card1
+      </div>
     </>
   );
 };
--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
     SIGN_UP: "/sign-up",
     PROFILE: (id: string) => `/profile/${id}`,
     TAGS: (id: string) => `/tags/${id}`,
+    ASK_QUESTION: "/ask-question",
 }
 
 
