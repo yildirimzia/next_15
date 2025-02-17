164d481d2b622c8f0648a3ce97a5a297fb07042d
ziya yildirim <zia.yildirimm@gmail.com>
2025-02-17 13:22:10 +0300

    Implement Question card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import QuestionCard from "@/components/cards/QuestionCard";
 
 const questions = [
   {
@@ -14,7 +15,12 @@ const questions = [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
     upvotes: 10,
     answers: 5,
     views: 100,
@@ -28,11 +34,16 @@ const questions = [
       { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
     upvotes: 10,
     answers: 5,
     views: 100,
-    createdAt: new Date(),
+    createdAt: new Date("2021-09-01"),
   },
 ];
 
@@ -76,7 +87,7 @@ const Home = async ({ searchParams }: SearchParams) => {
       <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <h1 key={question._id}>{question.title}</h1>
+          <QuestionCard key={question._id} question={question} />
         ))}
       </div>
     </>
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,11 +2,10 @@ const ROUTES = {
     HOME: "/",
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
-    PROFILE: (id: string) => `/profile/${id}`,
-    TAGS: (id: string) => `/tags/${id}`,
     ASK_QUESTION: "/ask-question",
-}
-
-
-
-export default ROUTES   
+    PROFILE: (id: string) => `/profile/${id}`,
+    QUESTION: (id: string) => `/question/${id}`,
+    TAGS: (id: string) => `/tags/${id}`,
+  };
+  
+  export default ROUTES;
